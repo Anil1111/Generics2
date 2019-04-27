9ac05767d527d79e9dc2a90ff3e814eaf3d9c79b
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-04-27 16:56:51 +0530

    Generics

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp12
-{
-    class Program
+   class Program
     {
         static void Main(string[] args)
         {
+            var langs = new List<string>();
+
+            langs.Add("Pascal");
+            langs.Add("Simula");
+            langs.Add("Pearl");
+            langs.Add("Java");
+            langs.Add("C#");
+            langs.Add("C");
+            langs.Add("C++");
+            langs.Add("Ruby");
+            langs.Add("Javascript");
+
+            
+
+            langs.Remove("C++");
+           
+
+            Console.WriteLine(langs.Contains("C#"));
+
+            langs.Insert(4, "Haskell");
+
+            langs.Sort();
+
+            foreach (string lang in langs)
+            {
+                Console.WriteLine(lang);
+            }
         }
     }
-}
+
